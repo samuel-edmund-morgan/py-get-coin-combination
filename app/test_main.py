8ad48a02c4fec2fa926e0bd15b4a55b9783dce01
samuel-edmund-morgan <samuel.edmund.morgan@nasbu.edu.ua>
2024-09-13 12:56:03 +0300

    Solution, tests passed, flake8 - ok.

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,34 @@
 from app.main import get_coin_combination
+import pytest
 
 
-# write your tests here
+class TestGetCoinCombination:
+    @pytest.mark.parametrize(
+        "cents_input,result_list",
+        [
+            pytest.param(
+                41, [1, 1, 1, 1],
+                id="Should fill all resulting list"
+            ),
+            pytest.param(
+                40, [0, 1, 1, 1],
+                id="Should fill all resulting list except penny"
+            ),
+            pytest.param(
+                35, [0, 0, 1, 1],
+                id="Should fill all resulting list except nickel"
+            ),
+            pytest.param(
+                25, [0, 0, 0, 1],
+                id="Should fill all resulting list except dime"
+            ),
+            pytest.param(
+                0, [0, 0, 0, 0],
+                id="Should return empty list"
+            )
+        ]
+    )
+    def test_get_coin_combination(self,
+                                  cents_input: int,
+                                  result_list: list[int]) -> None:
+        assert get_coin_combination(cents_input) == result_list
